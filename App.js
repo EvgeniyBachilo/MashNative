ae49bf8ff9d6582765e0e9607ea9f2fcd74268c1
Zhenya <e.bachilo@nord-soft.com>
2022-05-20 23:56:05 +0300

    created card component

--- App.js
+++ App.js
@@ -3,7 +3,20 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import Jacket from "./app/assets/jacket.jpg";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card title="Red jacket for sale" subtitle="$100" image={Jacket} />
+    </View>
+  );
 }
