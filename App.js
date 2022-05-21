567ee95f164192e36630fcd9b4bbf50ae6cd83d8
Zhenya <e.bachilo@nord-soft.com>
2022-05-21 14:53:09 +0300

    commit

--- App.js
+++ App.js
@@ -5,18 +5,21 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import Jacket from "./app/assets/jacket.jpg";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#f8f4f4",
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <Card title="Red jacket for sale" subtitle="$100" image={Jacket} />
-    </View>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     backgroundColor: "#f8f4f4",
+    //     padding: 20,
+    //     paddingTop: 100,
+    //   }}
+    // >
+    //   <Card title="Red jacket for sale" subtitle="$100" image={Jacket} />
+    // </View>
+    // <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Image, View, StyleSheet } from "react-native";
+
+import Jacket from "../assets/jacket.jpg";
+import AppText from "../components/AppText";
+import colors from "../config/colors";
+import Proto from "../assets/mosh.jpg";
+import ListItem from "../components/ListItem";
+
+const ListingDetailsScreen = () => {
+  return (
+    <View>
+      <Image style={styles.image} source={Jacket} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>Jacket for sale</AppText>
+        <AppText style={styles.price}>100$</AppText>
+        <View style={styles.userContainer}>
+          <ListItem image={Proto} title="Mosh Hamedani" subtitle="5 listings" />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ListingDetailsScreen;
+
+const styles = StyleSheet.create({
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
+  price: {
+    color: colors.secondary,
+    fontWeight: "bold",
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  userContainer: {
+    marginVertical: 40,
+  },
+});
