175be9cfbc7bf60f5f1459aeaab4c884ebb79e96
Zhenya <e.bachilo@nord-soft.com>
2022-05-24 10:51:05 +0300

    created textinput component

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput icon="email" placeholder={"user"} />
+    </Screen>
+  );
 }
