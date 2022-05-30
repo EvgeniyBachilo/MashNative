acd5d4c9c06453c0c35c4afec12e501739002639
Zhenya <e.bachilo@nord-soft.com>
2022-05-30 13:43:15 +0300

    added imagePicker

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button, Image } from "react-native";
 
 import ListingsScreen from "./app/screens/ListingsScreen";
 import Screen from "./app/components/Screen";
@@ -7,10 +8,35 @@ import AppPicker from "./app/components/AppPicker";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
-
-
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+  const requestPermission = async () => {
+    const result = await ImagePicker.getMediaLibraryPermissionsAsync(true);
+    if (!result.granted) {
+      alert("give a permission!!");
+    }
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
+    } catch (e) {
+      console.log("Error choosing image");
+    }
+  };
 
-  return <ListingEditScreen />;
+  return (
+    <Screen>
+      <Button title={"Add image"} onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
