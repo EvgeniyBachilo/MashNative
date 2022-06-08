8f4c55b6197bcc969c7881cbefb707c31c92bfc2
Zhenya <e.bachilo@nord-soft.com>
2022-06-08 12:46:50 +0300

    updated expo sdk

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: "http://192.168.0.104:9000/api",
+  baseURL: "http://192.168.1.43:9000/api",
 });
 
 export default apiClient;
